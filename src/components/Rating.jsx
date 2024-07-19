1bd00d61edaa139b12c4609275a71050b540f68e
My Name <hishamrehab309@gmail.com>
2024-07-20 02:04:27 +0300

    Our Products Section

---
+++ src/components/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Rating = () => {
+    return (
+        <span className='rating'>
+            <i className='icofont-ui-rating'></i>
+            <i className='icofont-ui-rating'></i>
+            <i className='icofont-ui-rating'></i>
+            <i className='icofont-ui-rating'></i>
+            <i className='icofont-ui-rating'></i>
+        </span>
+    )
+}
+
+export default Rating
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
+import CategoryShowCase from './CategoryShowCase'
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <HomeCategory />
+            <CategoryShowCase />
         </div>
     )
 }
